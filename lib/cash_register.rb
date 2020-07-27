15ab384077bbe1b6d9539bd5b83765b290d31f36
Elaine Ramirez <elaineramirez208@yahoo.com>
2020-07-27 22:18:49 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,22 +17,15 @@ class CashRegister
     self.total = cost * number
 
   def apply_discount 
-  # the cash register was initialized with an employee discount:  
-    # applies the discount to the total price
-    # returns success message with updated total
-    # reduces the total
-    
-  # the cash register was *NOT* initialized with an employee discount:
-    # returns a string error message that there is no discount to appply
+ 
   end 
   
   def items 
-  # (self).items!?
-  # returns an array containing all items that have been added
+  
   end
   
   def void_last_transaction
-  # subtracts the last transaction from the total 
+ 
   end 
   
 end 
